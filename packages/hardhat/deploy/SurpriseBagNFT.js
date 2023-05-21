1828f75349d310b4e9fdf03a347f6d39bd576445
davidtnfsh <davidtnfsh@gmail.com>
2023-05-21 11:47:28 +0200

    [fix]: deploy need to wait

--- packages/hardhat/deploy/SurpriseBagNFT.js
+++ packages/hardhat/deploy/SurpriseBagNFT.js
@@ -3,14 +3,17 @@ const hre = require("hardhat");
 async function main() {
   const MockWEth = await hre.ethers.getContractFactory("MockWEth");
   let mockWEth = await MockWEth.deploy();
+  await mockWEth.deployed();  
   console.log('Deployed mockWEth Address:', mockWEth.address);
 
   const TokenizedVault = await hre.ethers.getContractFactory("TokenizedVault");
   let tokenizedVault = await TokenizedVault.deploy(mockWEth.address);
+  await tokenizedVault.deployed();
   console.log('Deployed tokenizedVault Address:', tokenizedVault.address);
   
   const SurpriseBagNFT = await hre.ethers.getContractFactory("SurpriseBagNFT");
   let surpriseBagNFT = await SurpriseBagNFT.deploy("0x02101dfB77FDE026414827Fdc604ddAF224F0921", mockWEth.address, tokenizedVault.address);
+  await surpriseBagNFT.deployed();
   console.log('Deployed surpriseBagNFT Address:', surpriseBagNFT.address);
 }
 
